88b0636db6c0fb2767d93d5487c34adc4512a241
Veerasiva <96305584+Veera5iva@users.noreply.github.com>
2024-10-26 19:09:39 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,16 +6,21 @@ from tensorflow.keras.preprocessing import image  # Corrected import
 
 model = tf.keras.models.load_model('trained_model.h5')
 
-st.markdown("<h1 style='text-align: center;'>Character Recognition: 'V' or Not 'V'</h1>", unsafe_allow_html=True)
+# Reduced font size for the heading
+st.markdown(
+    "<h1 style='text-align: center; font-size: 32px;'>Character Recognition: 'V' or Not 'V'</h1>",
+    unsafe_allow_html=True
+)
 st.write("Upload an image to predict whether it's the character 'V' or not.")
 
-def predict_image(image):
-    image = np.array(image)
-    image = tf.image.resize(image, (64, 64))
-    image = image / 255.0
-    image = np.expand_dims(image, axis=0)
+def predict_image(img):
+    img = img.convert("RGB")  # Ensure the image is in RGB mode
+    img = np.array(img)
+    img = tf.image.resize(img, (64, 64))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)  # Add batch dimension
 
-    prediction = model.predict(image)
+    prediction = model.predict(img)
     if prediction[0][0] > 0.5:
         predicted_class = 'V'
         background_color = 'green'
